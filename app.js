6d94ddecd6b3efd56c29026b8870487de8c4fa6d
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-06 14:05:44 -0500

    Added userName input and confirmation to app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('loaded app.js');
+
+
+do{
+  var userName = prompt('Please tell me your name.' , 'Harold Bluetooth'); //get userName default to 'Harold Bluetooth'
+  console.log('user input '+userName+ ' for "userName"');
+  var correctName = confirm('You name is '+userName+ ' Is that correct?'); //confirm userName before continuing
+  console.log('user input ' +correctName+ ' for "correctName"');
+}while (!correctName);
