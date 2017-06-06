09f262516e4790a02ff0c332582b43dda8ee8735
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-06 14:15:07 -0500

    added wantsToPlay confirmation to app.js

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ do{
   var correctName = confirm('You name is '+userName+ ' Is that correct?'); //confirm userName before continuing
   console.log('user input ' +correctName+ ' for "correctName"');
 }while (!correctName);
+
+var wantsToPlay = confirm('Nice to meet you '+userName+'! Would you like to take a quick quiz about me? (click OK to continue.)');//confirm user want to play game
+
+if(!wantsToPlay){ // end game if user doesn't want to play
+  alert('OK, goodbye!');
+  console.log('"wantsToPlay" = '+wantsToPlay+ ' Ending game');
+}
+else{
+  console.log('"wantsToPlay" = '+wantsToPlay+ ' Continuing game');
+}
